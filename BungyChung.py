23c1d8d61b0e147b5732e897062a40329511e5d5
BungyChung <331704@guhsd.net>
2019-10-28 20:25:21 +0000

    Added BungyChung and addded to the main program

---
+++ BungyChung.py
@@ -0,0 +1,19 @@
+####
+# Each team's file must define four tokens:
+#     team_name: a string
+#     strategy_name: a string
+#     strategy_description: a string
+#     move: A function that returns 'c' or 'b'
+####
+
+team_name = 'Justin Taing'
+strategy_name = 'Stop Snitching'
+strategy_description = 'Start out with a betray, if they betray you, switch to always betray'
+    
+def move(my_history, their_history, my_score, their_score):
+  if len(my_history) == 0:
+    return 'b'
+  else:
+    return 'c'
+
+    
