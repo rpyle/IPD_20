791196cc9e777094c46e966a49cb2627305e5d6c
Justin Lee <329124@guhsd.net>
2019-10-29 06:49:43 +0000

    Implemented justin_lee_master.py

--- justin_lee_master.py
+++ justin_lee_master.py
@@ -1,8 +1,9 @@
 team_name = 'Justin Lee'
-strategy_name = 'spiteful_cc'
-strategy_description = 'For the first two rounds, always collude. If the enemy ever betrays after those two collusions, always betray from that point.'
+strategy_name = 'spiteful_cc/slavemaster'
+strategy_description = 'Plays spiteful_cc strategy until it identifies a slave, then switches to slavemaster strategy.'
 
-slaveIdentifier = ['c', 'c', 'b', 'c', 'b', 'c']
+slaveIdentifier  = ['c', 'c', 'b', 'b', 'c', 'c', 'c', 'c']
+masterIdentifier = ['c', 'c', 'c', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c']
 
 def move(my_history, their_history, my_score, their_score):
   isOpponentSlave = False
@@ -11,5 +12,16 @@ def move(my_history, their_history, my_score, their_score):
     for x in range(len(slaveIdentifier)):
       if not their_history[x] == slaveIdentifier[x]:
         isOpponentSlave = False
-  
-  
+  if isOpponentSlave == True:
+    if len(my_history) < len(masterIdentifier):
+      return masterIdentifier[len(my_history)]
+    else:
+      return 'b'
+  else:
+    if len(my_history) <= 1:
+      return 'c'
+    else:
+      if 'b' in their_history:
+        return 'b'
+      else:
+        return 'c'
