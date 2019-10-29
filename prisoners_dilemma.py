342ec3a5bbdb16ee49b4fd3c7b268ad70ea9b0a8
GrantJac <331971@guhsd.net>
2019-10-29 20:30:32 +0000

    Commit 3

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -35,12 +35,12 @@ import os.path
     
 import example0, example1, example2, example3
 import example4, example5, example6, example7, example8
-
+import Grant
 #betray = example1
 #collude = example0
 
 modules = [example0, example1, example2, example3, example4, example5, example6, example7,
-           example8]
+           example8, Grant]
 
 for module in modules:
     reload(module)
