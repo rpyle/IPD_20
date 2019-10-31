336ccf359de1a94dc5ac1221af007ac1f61f0d83
Jam'nt <Jbuksa711@gmail.com>
2019-10-31 19:39:10 +0000

    Added _____.py

--- _____.py
+++ _____.py
@@ -6,7 +6,7 @@ If I haven't been betrayed yet, I'll collude.
 '''
 
 def move(my_history, their_history, my_score, their_score):
-    if 'b' in their_history > 0: # If I am betrayed 
+    if 'b' in their_history > 0: # If I am betrayed
         return 'b'               # Betray.
     else:
         return 'c'               # Collude
