5ed8945f65c58588b41353fe78f50705c3a5d75d
329124 <329124@guhsd.net>
2019-10-31 20:20:46 +0000

    Cleaned up code for master and slave. Fixed prisoners_dilemma.py

--- justin_lee_master.py
+++ justin_lee_master.py
@@ -2,39 +2,39 @@ team_name = 'Justin Lee'
 strategy_name = 'spiteful_cc/slavemaster'
 strategy_description = 'Plays spiteful_cc strategy until it identifies a slave, then switches to slavemaster strategy. Identifies its own slaves and also tricks opposing players slaves into feeding self.'
 
-slaveIdentifier  = ['c', 'c', 'b', 'b', 'c', 'c', 'c']
-masterIdentifier = ['c', 'c', 'c', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c']
-devinSlaveIdentifier  = ['c', 'b', 'b', 'b', 'b', 'c']
-devinMasterIdentifier = ['c', 'c', 'b', 'b', 'b', 'b', 'c', 'b', 'c']
+justinSlaveIdentifier  = ['c', 'c', 'b', 'c', 'b', 'c']
+devinSlaveIdentifier   = ['c', 'b', 'b', 'b', 'b', 'c']
+
+justinMasterIdentifier = ['c', 'c', 'c', 'b', 'b', 'b', 'c', 'c', 'b']
+devinMasterIdentifier  = ['c', 'c', 'b', 'b', 'b', 'b', 'c', 'b', 'c']
+
+def checkSlave(their_history, slaveIdentifier):
+    if len(their_history) < len(slaveIdentifier):
+        return False
+    for x in range(len(slaveIdentifier)):
+        if not their_history[x] == slaveIdentifier[x]:
+            return False
+    return True
+
+def followIdentifier(my_history, identifier):
+    if len(my_history) < len(identifier):
+        return identifier[len(my_history)]
+    return False
+
+def spiteful_cc(their_history):
+    if len(their_history) <= 1:
+        return 'c'
+    if 'b' in their_history:
+        return 'b'
+    return 'c'
 
 def move(my_history, their_history, my_score, their_score):
-  isOpponentSlave = False
-  if len(their_history) >= len(slaveIdentifier):
-    isOpponentSlave = True
-    for x in range(len(slaveIdentifier)):
-      if not their_history[x] == slaveIdentifier[x]:
-        isOpponentSlave = False
-  isOpponentDevinSlave = False
-  if len(their_history) >= len(devinSlaveIdentifier):
-    isOpponentDevinSlave = True
-    for x in range(len(devinSlaveIdentifier)):
-      if not their_history[x] == devinSlaveIdentifier[x]:
-        isOpponentDevinSlave = False
-  if isOpponentDevinSlave == True:
-    if len(my_history) < len(devinMasterIdentifier):
-      return devinMasterIdentifier[len(my_history)]
-    else:
-      return 'b'
-  elif isOpponentSlave == True:
-    if len(my_history) < len(masterIdentifier):
-      return masterIdentifier[len(my_history)]
-    else:
-      return 'b'
-  else:
-    if len(my_history) <= 1:
-      return 'c'
-    else:
-      if 'b' in their_history:
-        return 'b'
-      else:
-        return 'c'
+    if checkSlave(their_history, justinSlaveIdentifier):
+        if not followIdentifier(my_history, justinMasterIdentifier):
+            return 'b'
+        return followIdentifier(my_history, justinMasterIdentifier)
+    if checkSlave(their_history, devinSlaveIdentifier):
+        if not followIdentifier(my_history, devinMasterIdentifier):
+            return 'b'
+        return followIdentifier(my_history, devinMasterIdentifier)
+    return spiteful_cc(their_history)
--- justin_lee_slave.py
+++ justin_lee_slave.py
@@ -2,17 +2,26 @@ team_name = 'Justin Lee'
 strategy_name = 'j_slave'
 strategy_description = 'Identifies slavemaster and feeds them.'
 
-slaveIdentifier  = ['c', 'c', 'b', 'b', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c']
-masterIdentifier = ['c', 'c', 'c', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c']
+justinSlaveIdentifier  = ['c', 'c', 'b', 'c', 'b', 'c', 'c', 'c', 'c']
+justinMasterIdentifier = ['c', 'c', 'c', 'b', 'b', 'b', 'c', 'c', 'b']
+
+def checkMaster(their_history, masterIdentifier):
+    if len(their_history) < len(masterIdentifier):
+        return False
+    for x in range(len(masterIdentifier)):
+        if not their_history[x] == masterIdentifier[x]:
+            return False
+    return True
+
+def followIdentifier(my_history, identifier):
+    if len(my_history) < len(identifier):
+        return identifier[len(my_history)]
+    return False
 
 def move(my_history, their_history, my_score, their_score):
-  if len(my_history) < len(slaveIdentifier):
-    return slaveIdentifier[len(my_history)]
-  isOpponentMaster = True
-  for x in range(len(masterIdentifier)):
-    if not their_history[x] == masterIdentifier[x]:
-      isOpponentMaster = False
-  if isOpponentMaster == True:
-    return 'c'
-  else:
-    return 'b'
+    if not followIdentifier(my_history, justinSlaveIdentifier):
+        if checkMaster(their_history, justinMasterIdentifier):
+            return 'c'
+        else:
+            return 'b'
+    return followIdentifier(my_history, justinSlaveIdentifier)
