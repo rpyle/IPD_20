8b9a571abeb26300e86346f5a83fea739a43df3f
xXJacobMXx <328863@guhsd.net>
2019-10-31 19:48:04 +0000

    added jacob.py

--- jacob.py
+++ jacob.py
@@ -9,16 +9,16 @@
 team_name = 'Jacob'
 strategy_name = 'Insert Strat Here'
 strategy_description = 'Describe Strat'
-    
+
 def move(my_history, their_history, my_score, their_score):
     '''Make my move based on the history with this player.
-    
+
     history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
+    their_history: a string of the same length as history, possibly empty.
     The first round between these two players is my_history[0] and their_history[0]
     The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
+
+    Returns 'c' or 'b' for collude or betray
     '''
 
     if len(my_history) == 0:
