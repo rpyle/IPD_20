7cba29e4abecca128347d66b04ac52bc362fba6e
BungyChung <331704@guhsd.net>
2019-10-31 19:45:19 +0000

    Redo

--- BungyChung.py
+++ BungyChung.py
@@ -9,11 +9,14 @@
 team_name = 'Justin Taing'
 strategy_name = 'Stop Snitching'
 strategy_description = 'Start out with a betray, if they betray you, switch to always betray'
-    
+
 def move(my_history, their_history, my_score, their_score):
   if len(my_history) == 0:
+    return 'c'
+  elif len(my_history) < 3:
+    return 'b'
+  elif their_history[-1] or their_history[-2] == 'c':
     return 'b'
   else:
-    return 'c'
+    return their_history[-1]
 
-    
