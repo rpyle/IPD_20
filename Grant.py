ddd15128e35a4da6dc7e838fa9d8951c1a31d5bc
GrantJac <331971@guhsd.net>
2019-11-01 20:18:28 +0000

    Final

--- Grant.py
+++ Grant.py
@@ -1,18 +1,15 @@
-from random import *
 team_name = 'Grant'
-strategy_name = 'Copy 5'
-strategy_description = ''
-    
+strategy_name = 'Forgive25'
+strategy_description = 'If betrayed in the last twentyfive moves betray'
+
 def move(my_history, their_history, my_score, their_score):
 
   if len(their_history)==0:
     return 'c'
 
-  elif len(their_history) <25:
-    return their_history[-1]
-
+  elif len(their_history) <25 and 'b' in their_history:
+    return 'b'
   elif 'b' in their_history[-25:]:
     return 'b'
-
   else:
     return 'c'
